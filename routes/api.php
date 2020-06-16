eea6106a6a484a5d1aee77a523fd09541c85f4c7
khechinimed <khechinibakr20@gmail.com>
2020-06-16 06:43:43 +0100

    add api route for Class

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:api')->get('/class', function (Request $request) {
+    return $request->class();
+});
+
 Route::apiResources(['user'=> 'API\UserController']);
 Route::get('profile', 'API\UserController@profile');
 Route::get('findUser', 'API\UserController@search');
 Route::put('profile', 'API\UserController@updateProfile');
+
+Route::apiResources(['class'=> 'API\ClassController']);
