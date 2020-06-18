87942b47720bba7a2477b6c9a2a2e01088b722a7
khechinimed <khechinibakr20@gmail.com>
2020-06-18 07:34:49 +0100

    added Teachers component for the admin

--- resources/js/components/Teacher.vue
+++ resources/js/components/Teacher.vue
@@ -1,19 +1,74 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Teacher Component</div>
+        <div class="row mt-4">
+            <div class="col-md-3" v-for="user in users.data" :key="user.id">
+            <!-- Widget: user widget style 2 -->
+            <div class="card card-widget widget-user-2">
+              <!-- Add the bg color to the header using any of the bg-* classes -->
+              <div class="widget-user-header bg-warning" style="height: 100px !important">
+                <div class="widget-user-image">
+                  <img class="img-circle elevation-2" :src="'/img/profile/' + user.photo" alt="User Avatar">
                 </div>
+                <!-- /.widget-user-image -->
+                <h3 class="widget-user-username">{{ user.name }}</h3>
+                <h5 class="widget-user-desc">{{ user.type | upText}}</h5>
+              </div>
+              <div class="card-footer p-0">
+                <ul class="nav flex-column">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      Projects <span class="float-right badge bg-primary">31</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      Tasks <span class="float-right badge bg-info">5</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      Completed Projects <span class="float-right badge bg-success">12</span>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">
+                      Followers <span class="float-right badge bg-danger">842</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
+            <!-- /.widget-user -->
+          </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data() {
+            return{
+                editmode: false,
+                users: {},
+                form: new Form({
+                    id: '',
+                    name: '',
+                    email: '',
+                    password: '',
+                    bio: '',
+                    type: '',
+                    photo: ''
+                })
+            }
+        },
+
+        methods: {
+            loadUsers(){
+                axios.get('api/user').then(({ data }) => (this.users = data));
+            },
+        },
+        created() {
+            this.loadUsers();
         }
     }
 </script>
