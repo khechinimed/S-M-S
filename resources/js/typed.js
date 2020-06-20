038fc319d041096d220a78b4bedc8a279d9eb02e
khechinimed <khechinibakr20@gmail.com>
2020-06-20 04:16:46 +0100

    Typed  JS animation

---
+++ resources/js/typed.js
@@ -0,0 +1,14 @@
+
+var Typed = require('../../node_modules/typed.js/lib/typed.js');
+
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+//TYPED JS
+var typed = new Typed('#miage', {
+    strings: ['Méthodes informatiques appliquées à la gestion des entreprises.'],
+    typeSpeed: 90
+});
