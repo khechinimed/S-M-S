697ea48b2093e82a56585f2a91a2eaa347288c29
khechinimed <khechinibakr20@gmail.com>
2020-06-24 05:01:35 +0100

    delete a useless elements

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -11,37 +11,7 @@
                 <div class="widget-user-image">
                     <img class="img-circle elevation-2" :src="getProfilePhoto()" alt="User Avatar">
                 </div>
-                <div class="card-footer">
-                    <div class="row">
-                    <div class="col-sm-4 border-right">
-                        <div class="description-block">
-                        <h5 class="description-header">3,200</h5>
-                        <span class="description-text">SALES</span>
-                        </div>
-                        <!-- /.description-block -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-4 border-right">
-                        <div class="description-block">
-                        <h5 class="description-header">13,000</h5>
-                        <span class="description-text">FOLLOWERS</span>
-                        </div>
-                        <!-- /.description-block -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-sm-4">
-                        <div class="description-block">
-                        <h5 class="description-header">35</h5>
-                        <span class="description-text">PRODUCTS</span>
-                        </div>
-                        <!-- /.description-block -->
-                    </div>
-                    <!-- /.col -->
-                    </div>
-                    <!-- /.row -->
                 </div>
-                </div>
-
 
                 <!-- SECOND PART -->
             <div class="card">
