727a68c996120c9c169ec5f03c787d81c261574b
khechinimed <khechinibakr20@gmail.com>
2020-07-16 04:54:34 +0100

    Notes migration

---
+++ database/migrations/2020_07_16_003622_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->double('note1', 8, 2);
+            $table->double('note2', 8, 2);
+            $table->double('note_finale', 8, 2);
+            $table->integer('student_id');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
