df5477d1222c49b070721089e2d02bba950ae9a2
khechinimed <khechinibakr20@gmail.com>
2020-07-21 10:11:34 +0100

    Printing student marks page

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -11,8 +11,8 @@
                           <div class="row">
                             <div class="col-12">
                               <h4>
-                                <i class="fa fa-globe"></i> AdminLTE, Inc.
-                                <small class="float-right">Date: 2/10/2018</small>
+                                <i class="fa fa-globe"></i> MIAGE | 2020
+                                <small class="float-right">Date: 21/07/2020</small>
                               </h4>
                             </div>
                             <!-- /.col -->
@@ -31,17 +31,6 @@
                             </div>
                             <!-- /.col -->
                             <div class="col-sm-4 invoice-col">
-                              To
-                              <address>
-                                <strong>John Doe</strong><br>
-                                795 Folsom Ave, Suite 600<br>
-                                San Francisco, CA 94107<br>
-                                Phone: (555) 539-1037<br>
-                                Email: john.doe@example.com
-                              </address>
-                            </div>
-                            <!-- /.col -->
-                            <div class="col-sm-4 invoice-col">
                               <b>Invoice #007612</b><br>
                               <br>
                               <b>Order ID:</b> 4F3S8J<br>
@@ -53,110 +42,61 @@
                           <!-- /.row -->
 
                           <!-- Table row -->
-                          <div class="row">
-                            <div class="col-12 table-responsive">
-                              <table class="table table-striped">
-                                <thead>
-                                <tr>
-                                  <th>Qty</th>
-                                  <th>Product</th>
-                                  <th>Serial #</th>
-                                  <th>Description</th>
-                                  <th>Subtotal</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                  <td>1</td>
-                                  <td>Call of Duty</td>
-                                  <td>455-981-221</td>
-                                  <td>El snort testosterone trophy driving gloves handsome</td>
-                                  <td>$64.50</td>
-                                </tr>
-                                <tr>
-                                  <td>1</td>
-                                  <td>Need for Speed IV</td>
-                                  <td>247-925-726</td>
-                                  <td>Wes Anderson umami biodiesel</td>
-                                  <td>$50.00</td>
-                                </tr>
-                                <tr>
-                                  <td>1</td>
-                                  <td>Monsters DVD</td>
-                                  <td>735-845-642</td>
-                                  <td>Terry Richardson helvetica tousled street art master</td>
-                                  <td>$10.70</td>
-                                </tr>
-                                <tr>
-                                  <td>1</td>
-                                  <td>Grown Ups Blue Ray</td>
-                                  <td>422-568-642</td>
-                                  <td>Tousled lomo letterpress</td>
-                                  <td>$25.99</td>
-                                </tr>
-                                </tbody>
-                              </table>
-                            </div>
-                            <!-- /.col -->
-                          </div>
-                          <!-- /.row -->
 
-                          <div class="row">
-                            <!-- accepted payments column -->
-                            <div class="col-6">
-                              <p class="lead">Payment Methods:</p>
-                              <img src="https://adminlte.io/themes/dev/AdminLTE/dist/img/credit/visa.png" alt="Visa">
-                              <img src="https://adminlte.io/themes/dev/AdminLTE/dist/img/credit/mastercard.png" alt="Mastercard">
-                              <img src="https://adminlte.io/themes/dev/AdminLTE/dist/img/credit/american-express.png" alt="American Express">
-                              <img src="https://adminlte.io/themes/dev/AdminLTE/dist/img/credit/paypal2.png" alt="Paypal">
-
-                              <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
-                                Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                                plugg
-                                dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                              </p>
+                            <div class="row justify-content-center">
+                                <div class="col-md-12 pt-4">
+                                    <h3 class="card-title">Notes</h3>
+                                    <table class="table">
+                                        <thead class="thead-dark">
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Prenom</th>
+                                                <th>Nom</th>
+                                                <th>Age</th>
+                                                <th>Filière</th>
+                                                <th>Note 1</th>
+                                                <th>Note 2</th>
+                                                <th>Note finale</th>
+                                                <th>Registred At</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th>01</th>
+                                                <th>MOHAMED</th>
+                                                <th>KHECHINI</th>
+                                                <th>21</th>
+                                                <th>MIAGE</th>
+                                                <th>16.75</th>
+                                                <th>18</th>
+                                                <th>17.37</th>
+                                                <th>21 - 07 - 2020</th>
+                                            </tr>
+                                            <tr>
+                                                <th>16</th>
+                                                <th>AZIZ</th>
+                                                <th>WALOU</th>
+                                                <th>20</th>
+                                                <th>MIAGE</th>
+                                                <th>12</th>
+                                                <th>10</th>
+                                                <th>11</th>
+                                                <th>21 - 07 - 2020</th>
+                                            </tr>
+                                    
+                                        </tbody>
+                                        </table>
+                                </div>
                             </div>
-                            <!-- /.col -->
-                            <div class="col-6">
-                              <p class="lead">Amount Due 2/22/2014</p>
+                        </div>
 
-                              <div class="table-responsive">
-                                <table class="table">
-                                  <tbody><tr>
-                                    <th style="width:50%">Subtotal:</th>
-                                    <td>$250.30</td>
-                                  </tr>
-                                  <tr>
-                                    <th>Tax (9.3%)</th>
-                                    <td>$10.34</td>
-                                  </tr>
-                                  <tr>
-                                    <th>Shipping:</th>
-                                    <td>$5.80</td>
-                                  </tr>
-                                  <tr>
-                                    <th>Total:</th>
-                                    <td>$265.24</td>
-                                  </tr>
-                                </tbody></table>
-                              </div>
-                            </div>
-                            <!-- /.col -->
-                          </div>
-                          <!-- /.row -->
+                          
 
                           <!-- this row will not appear when printing -->
                           <div class="row no-print">
                             <div class="col-12">
-
-                              <a href="" @click.prevent="printme" target="_blank" class="btn btn-default"><i class="fa fa-print"></i> Print</a>
-                              <button type="button" class="btn btn-success float-right">
-                                  <i class="fa fa-credit-card"></i>
-                                  Submit Payment
-                              </button>
-
-                              <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
-                                <i class="fa fa-download"></i> Generate PDF
+                              <button @click.prevent="printme" target="_blank" type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
+                                <i class="fa fa-download"></i> Print / Generate PDF
                               </button>
 
                             </div>
